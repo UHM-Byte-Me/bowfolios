ee11b48a4b9289f07727b54a05b37cafc2e83403
Gum San Aung <aunggum@hawaii.edu>
2020-09-12 21:56:19 -1000

    basic auth setup

---
+++ lib/services/auth.dart
@@ -0,0 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Sign in with email and password
+  Future signInEmail(email, pwd) async {
+    try {
+      UserCredential result =
+          await _auth.signInWithEmailAndPassword(email: email, password: pwd);
+      return result.user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // Register with email and password
+  Future registerWithEmail(email, pwd) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: pwd);
+      return result.user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // Sign out
+  void signOut() {
+    try {
+      _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
