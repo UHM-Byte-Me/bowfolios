0c0afbddfb5ac6f03bfcc66615f062ca0e19f37d
Gum San Aung <aunggum@hawaii.edu>
2020-09-13 21:52:46 -1000

    added email auth

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -3,29 +3,43 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  Stream<User> get userStream {
+    return _auth
+        .authStateChanges()
+        .map((event) => FirebaseAuth.instance.currentUser);
+  }
+
   // Sign in with email and password
-  Future signInEmail(email, pwd) async {
+  Future logInWithEmail(email, pwd) async {
     try {
       UserCredential result =
           await _auth.signInWithEmailAndPassword(email: email, password: pwd);
       return result.user;
     } catch (e) {
-      print(e.toString());
-      return null;
+      // print(e.toString());
+      return Future.error(e.toString());
     }
   }
 
   // Register with email and password
-  Future registerWithEmail(email, pwd) async {
+  Future signUpWithEmail(email, pwd) async {
     try {
       UserCredential result = await _auth.createUserWithEmailAndPassword(
           email: email, password: pwd);
       return result.user;
     } catch (e) {
-      print(e.toString());
-      return null;
+      // print(e.toString());
+      return Future.error(e.toString());
     }
   }
 
   // Sign out
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      // print(e.toString());
+      return Future.error(e.toString());
+    }
+  }
 }
