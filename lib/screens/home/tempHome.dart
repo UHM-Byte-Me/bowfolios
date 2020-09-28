0a76a0b6ed024bd34ce841c4b6d4470b58afaacd
Gum San Aung <aunggum@hawaii.edu>
2020-09-27 18:07:43 -1000

    add stream provider for user

--- lib/screens/home/tempHome.dart
+++ lib/screens/home/tempHome.dart
@@ -1,10 +1,18 @@
 import 'package:bowfolios/screens/home/image_placement.dart';
 import 'package:bowfolios/screens/home/text_section.dart';
 import 'package:bowfolios/services/auth.dart';
+import 'package:bowfolios/shared/models.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class TempHome extends StatelessWidget {
   Widget build(BuildContext context) {
+    final profile = Provider.of<Profile>(context);
+
+    if (profile != null) {
+      print(profile.id);
+    }
+
     return Scaffold(
       //insert a modular bar here
       appBar: AppBar(
--- lib/screens/landing.dart
+++ lib/screens/landing.dart
@@ -1,5 +1,7 @@
 import 'package:bowfolios/screens/authentication/authenticate.dart';
 import 'package:bowfolios/screens/home/tempHome.dart';
+import 'package:bowfolios/services/db.dart';
+import 'package:bowfolios/shared/models.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -8,9 +10,17 @@ class LandingWrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<User>(context);
-    print(user);
     if (user != null) {
-      return TempHome();
+      // print(user);
+
+      return MultiProvider(
+        providers: [
+          StreamProvider<Profile>.value(
+            value: DataService().profileStream(user.uid),
+          )
+        ],
+        child: TempHome(),
+      );
     } else {
       return Authenticate();
     }
