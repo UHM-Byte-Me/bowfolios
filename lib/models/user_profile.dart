f581290016bb126caa628f19d60f4b9afff1cf88
Arslan Rakhmankulov <31532955+arslan-r@users.noreply.github.com>
2020-09-28 20:36:35 -0700

    Added some models

---
+++ lib/models/user_profile.dart
@@ -0,0 +1,9 @@
+class UserProfile {
+  String firstName = '';
+  String lastName = '';
+  String bio = '';
+  String title = '';
+  String pictureUrl = '';
+  //insert interests
+  // insert Projects
+}
--- lib/screens/your_profile/your_profile.dart
+++ lib/screens/your_profile/your_profile.dart
@@ -1,7 +1,16 @@
+import 'package:bowfolios/models/user_profile.dart';
 import 'package:bowfolios/screens/home/custom_drawer.dart';
 import 'package:flutter/material.dart';
 
-class YourProfile extends StatelessWidget {
+class YourProfile extends StatefulWidget {
+  @override
+  _YourProfileState createState() => _YourProfileState();
+}
+
+class _YourProfileState extends State<YourProfile> {
+  final _userProfile = UserProfile();
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,7 +18,29 @@ class YourProfile extends StatelessWidget {
       appBar: AppBar(
         title: Text('Your Profile'),
       ),
-      body: Text('Your Profile body'),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+        child: Builder(
+          builder: (context) => Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(),
+                TextFormField(),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
